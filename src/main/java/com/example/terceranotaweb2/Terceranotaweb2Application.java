b42ff8d24037711db0b999b9e54349efde9c7c91
Pedro Molina <pealmo98@gmail.com>
2023-12-10 20:26:59 -0500

    Initializer maven

---
+++ src/main/java/com/example/terceranotaweb2/Terceranotaweb2Application.java
@@ -0,0 +1,13 @@
+package com.example.terceranotaweb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Terceranotaweb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Terceranotaweb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/terceranotaweb2/Terceranotaweb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.terceranotaweb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Terceranotaweb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
